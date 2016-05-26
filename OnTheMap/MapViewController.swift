a65a1970f25adad74b257e1e96c1fb7601a0b9c2
S.Reshetnikov <S.Reshetnikov@ofkbank.ru>
2016-05-26 14:42:20 +0300

    Added mapView

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by SVYAT on 26.05.16.
+//  Copyright © 2016 HiT2B. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
